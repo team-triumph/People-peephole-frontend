96ddb06af528f9c777d12194515dcdfd355b1299
Michael Marcinek <michaelmarcinek@gmail.com>
2015-06-17 14:06:35 -0400

    Initializes Git and sets up project!

---
+++ app/js/setup.js
@@ -0,0 +1,26 @@
+;(function (){
+
+  'use strict';
+
+  // Globals
+  window.app = {};
+  window.app.Models = {};
+  window.app.Collections = {};
+  window.app.Views = {};
+  window.app.Routers = {};
+
+  // Root URL
+  app.rootURL = '...';
+
+
+  $.ajaxSetup({
+
+    headers: {
+      "Authorization" : "12345" // whatever value is stored in the cookie
+    }
+
+  });
+
+
+
+}());
