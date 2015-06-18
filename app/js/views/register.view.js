e9e7c7275ea0e461c954e33875c5999ecebcfaf1
Michael Marcinek <michaelmarcinek@gmail.com>
2015-06-18 15:13:35 -0400

    Saves merge

---
+++ app/js/views/register.view.js
@@ -0,0 +1,50 @@
+;(function (){
+
+  'use strict';
+
+  app.Views.Reegister = new Backbone.Views.extend({
+
+    className: 'register',
+
+    template: hbs.register,
+
+    events: {
+      'submit #addUser' : 'addUser'
+    },
+
+    initialize : function (){
+      var args = options || {};
+
+      this.collection = args.collection;
+
+      this.render();
+      $('.container').html(this.el);
+    },
+
+    render: function(event){
+
+      event.preventDefault();
+
+      var self = this,
+          form = form.find('#userInfo').val(),
+          firstName = form.find('#userFirstName').val();
+          lastName =form.find('#userLastName').val();
+          email = form.find('#email').val();
+          password = form.find('#password').val();
+    }
+
+      var user = new app.Models.PeepholeUser ({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password
+      });
+
+      this.collection.add(user).save().success( function (){
+        self.render();
+      });
+
+
+  });
+
+}());
